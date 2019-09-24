169c3f883bd32b625cdfed873f6426921414dd2c
BinhVi <binhvttph07052@fpt.edu.vn>
2019-09-24 21:11:57 +0700

    lab 2 - them tren lop - chua xong

--- app/src/main/java/vn/poly/mob204/bookmanager_binhvttph07052/ListTheLoaiActivity.java
+++ app/src/main/java/vn/poly/mob204/bookmanager_binhvttph07052/ListTheLoaiActivity.java
@@ -3,21 +3,49 @@ package vn.poly.mob204.bookmanager_binhvttph07052;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.poly.mob204.bookmanager_binhvttph07052.adapter.TheLoaiAdapter;
+import vn.poly.mob204.bookmanager_binhvttph07052.model.TheLoai;
+
 public class ListTheLoaiActivity extends AppCompatActivity {
+    private RecyclerView rvTheLoai;
+    List<TheLoai> dsTheLoai;
+    TheLoaiAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_the_loai);
+        rvTheLoai = (RecyclerView) findViewById(R.id.rvTheLoai);
 
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //recycler view configuration
+        dsTheLoai=new ArrayList<>();
+        adapter=new TheLoaiAdapter(this, dsTheLoai);
+        rvTheLoai.setAdapter(adapter);
+        LinearLayoutManager vertical=new LinearLayoutManager(this);
+        rvTheLoai.setLayoutManager(vertical);
+
+        //fake du lieu
+        for (int i=0; i<30; i++) {
+            //tao doi tuong the loai moi
+            TheLoai theLoai=new TheLoai(String.valueOf(i), "Van hoc "+i,
+                    "Khong co mo ta", i);
+            //them vao list
+            dsTheLoai.add(theLoai);
+        }
     }
 
     @Override
---
+++ app/src/main/java/vn/poly/mob204/bookmanager_binhvttph07052/adapter/TheLoaiAdapter.java
@@ -0,0 +1,79 @@
+package vn.poly.mob204.bookmanager_binhvttph07052.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import vn.poly.mob204.bookmanager_binhvttph07052.R;
+import vn.poly.mob204.bookmanager_binhvttph07052.model.TheLoai;
+
+public class TheLoaiAdapter extends RecyclerView.Adapter<TheLoaiAdapter.ViewHolder> implements Filterable {
+    List<TheLoai> arrTheLoai;
+    Activity context;
+
+    public TheLoaiAdapter(Activity context, List<TheLoai> arrTheLoai) {
+        this.arrTheLoai = arrTheLoai;
+        this.context = context;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=this.context.getLayoutInflater();
+        View view=inflater.inflate(R.layout.item_theloai, parent, false);
+        ViewHolder viewHolder=new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TheLoai theLoai=arrTheLoai.get(position);
+
+        holder.img.setImageResource(R.drawable.cateicon);
+        holder.txtMaTheLoai.setText(theLoai.getMaTheLoai());
+        holder.txtTenTheLoai.setText(theLoai.getTenTheLoai());
+
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrTheLoai.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView img;
+        private TextView txtMaTheLoai;
+        private TextView txtTenTheLoai;
+        private ImageView imgDelete;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img = (ImageView) itemView.findViewById(R.id.ivIcon);
+            txtMaTheLoai = (TextView) itemView.findViewById(R.id.tvMaTheLoai);
+            txtTenTheLoai = (TextView) itemView.findViewById(R.id.tvTenTheLoai);
+            imgDelete = (ImageView) itemView.findViewById(R.id.ivDelete);
+        }
+    }
+}
