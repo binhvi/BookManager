935b3d581652fd4fb37284c0786223679db97053
BinhVi <binhvttph07052@fpt.edu.vn>
2019-10-07 05:55:50 +0700

    dang lam them hoa don

--- app/src/main/java/vn/poly/mob204/bookmanager_binhvttph07052/activity/HoaDonActivity.java
+++ app/src/main/java/vn/poly/mob204/bookmanager_binhvttph07052/activity/HoaDonActivity.java
@@ -6,25 +6,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import vn.poly.mob204.bookmanager_binhvttph07052.R;
 
 public class HoaDonActivity extends AppCompatActivity {
+    private EditText edNgayMua;
+    private Button picDate;
+    private Button btnADDHoaDon;
+
+    //thong tin
+    private String dateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoa_don);
+        addControls();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void addControls() {
+        edNgayMua = (EditText) findViewById(R.id.edNgayMua);
+        picDate = (Button) findViewById(R.id.picDate);
+        btnADDHoaDon = (Button) findViewById(R.id.btnADDHoaDon);
     }
 
     public void datePicker(View view) {
     }
 
     public void AddHoaDon(View view) {
-        Intent intent = new Intent(this, HoaDonChiTietActivity.class);
-        startActivity(intent);
+        //lay thong tin
+        dateString=edNgayMua.getText().toString().trim();
+        //validate: neu chua chon ngay -> dateString empty --> thong bao phai chon ngay
+        if (dateString.isEmpty()) {
+            Toast.makeText(
+                    this,
+                    R.string.pls_pick_date,
+                    Toast.LENGTH_SHORT
+            ).show();
+            return;
+        }
+        Toast.makeText(
+                this,
+                "pass validate",
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
